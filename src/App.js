568231a78bcac6cb720f5922a4826a754f78d4a6
Arghun-dev <a.mousanezhad@kian.digital>
2021-05-29 23:24:28 +0430

    added parcel bundler

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-const Pet = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, "Luna"),
-    React.createElement("h3", {}, "Dog"),
-    React.createElement("h3", {}, "Havanese"),
-  ])
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pet from './Pet';
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Adopt Me"),
-      [1, 2, 3, 4].map((i) => React.createElement("h2", {}, i)),
-      React.createElement(Pet),
-      React.createElement(Pet),
-      React.createElement(Pet),
-    ]
-  );
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt Me'),
+    [1, 2, 3, 4].map((i) => React.createElement('h2', {}, i)),
+    React.createElement(Pet, {
+      name: 'Arghun',
+      animal: 'Dog',
+      breed: 'Havanese',
+    }),
+    React.createElement(Pet),
+    React.createElement(Pet),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
